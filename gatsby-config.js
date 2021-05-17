27087a13d8dcb4a507b67e58c384931859e2c648
Stackbit Code Editor <projects@stackbit.com>
2021-05-17 21:31:06 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `gatsby-starter-point`,
+    title: `gatsby-starter-pointhh`,
     author: `George Brown`,
     description: `A gatsby starter for personal blog.`,
     siteUrl: `https://gatsby-starter-point.netlify.app/`,
