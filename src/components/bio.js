1d150c1c7712df2d54cc75a9b664394cd7b2fd5c
Stackbit Code Editor <projects@stackbit.com>
2021-11-29 18:03:49 +0000

    bio.js: changed

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,7 +38,7 @@ const Bio = () => {
         <Img fluid={data.placeholderImage.childImageSharp.fluid} alt="阿江" />
       </div>
       <p style={{ maxWidth: 180 }}>
-      diario di viaggio,/br>vuoto
+      diario di viaggio</br>vuoto
       </p>
     </div>
   )
