1955a4b76a57e078b92e7f0b88558764804c3034
Stackbit Code Editor <projects@stackbit.com>
2021-11-29 18:03:06 +0000

    bio.js: changed

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,9 +38,7 @@ const Bio = () => {
         <Img fluid={data.placeholderImage.childImageSharp.fluid} alt="阿江" />
       </div>
       <p style={{ maxWidth: 180 }}>
-      Siamo tutti alla ricerca di qualcosa
-      <br/>
-      che riempia un vuoto
+      void
       </p>
     </div>
   )
