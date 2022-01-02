4e6a9e0a655ff09f859e541e3a1e74ddd9d7a077
Stackbit Code Editor <projects@stackbit.com>
2022-01-02 15:07:25 +0000

    layout.js: changed

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,7 @@ class Layout extends React.Component {
             </a>{" "}
             &bull;{" "}
             <a
-              href="/"
+              href="#"
               target="_blank"
               rel="noopener noreferrer"
             >
